243751db2ab1a54c926343121825f502016ba1cd
Ying Liu <ying.liu@genesys.com>
2021-03-02 13:55:49 -0500

    added static file

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import * as express from 'express';
 const app = express();
 import * as bodyParser from 'body-parser';
- 
+import * as path from 'path';
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.get('/', function (req: express.Request, res: express.Response) {
-  res.send("Hello world");
-  console.log(req.body)
-})
+app.use('/', express.static(path.join(__dirname, '../static')))
 
 app.post('/message', function (req: express.Request, res: express.Response) {
     res.send(req.body)
     console.log(req.body)
   })
- 
+
 app.listen(process.env.PORT||3000);
+
